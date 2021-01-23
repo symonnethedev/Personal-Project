89de6317f2925391a3243979b606e947441f3eb8
symonne taylor <safiya@symonnes-mbp.home>
2021-01-23 14:35:39 +0000

    casa page imgs

--- client/src/casaPastis.js
+++ client/src/casaPastis.js
@@ -1,54 +1,79 @@
 import React from "react";
 import "./casaPastis.css";
 import villa1 from "./images/cp1.jpg";
+import cpk from "./images/cpk.jpeg";
+import cpl from "./images/cpl.jpeg";
+import cpb from "./images/cpb.jpeg";
 
 const casaPastis = () => {
   return (
     <div>
-      <img src={villa1} alt="image" class="casa" />
-      <div class="content">
-        <div className="title">
-          <h2 class="titleText">Welcome Casa pastis </h2>
-          <p>Spain, Balearic Islands, Ibiza</p>
+      <section>
+        <img src={villa1} alt="image" class="casa" />
 
-          <div className="textBox">
-            <p>
-              Located in Cala Llonga, a beautiful Ibizan resort-town with one of
-              the island’s largest beaches, a picturesque setting and plenty of
-              things to do in town. The four-bedroom villa itself adopts a
-              contemporary style washed in white to accompany its beautiful
-              modern interior. Guests will be able to enjoy the stunning
-              panoramic views of the surrounding hills that evoke a sense of
-              privacy amongst nature. The spacious, open-plan design makes
-              spending time inside a pleasure while the expansive glass windows
-              and sliding doors allow a great deal of light to shine through
-              from the famous Ibizan sun. The luminous living area is
-              luxuriously spacious, featuring a large, ultra-comfortable sofa,
-              flat screen television attached to the wall and various dining
-              tables. The American-style open kitchen features a bench island
-              and is fully equipped with any appliance that one may need during
-              their stay, and has access to the pool. The villa’s exterior is a
-              perfect setting for spending quality time outside, featuring a
-              gorgeous long pool with access from two bedrooms and the living
-              room. The property also features a landscaped garden and a pine
-              forest which contrast the beauty of the natural environment with
-              the modernity of the luxurious villa. The family-friendly area
-              boasts access to parasailing, a scuba-diving school, windsurfing
-              and water-skiing activities for those that plan on keeping active
-              on vacation while being strategically located close to Ibiza town
-              for those who want to experience the famous Ibizan nightlife. Cala
-              Llonga manages to maintain a quieter, more relaxed atmosphere,
-              making it a perfect option for those who need a relaxing escape
-              but also want plenty of fun options nearby.
-            </p>
-            <h2>Features</h2>
-            <p>
-              MAID, PRIVATE POOL, AIR CONDITIONING, HOUSEKEEPING, WI-FI,
-              BARBECUE and TV
-            </p>
+        <div class="content">
+          <div className="title">
+            <h2 class="titleText">Welcome Casa pastis </h2>
+            <p>Spain, Balearic Islands, Ibiza</p>
+
+            <div className="textBox">
+              <p>
+                Located in Cala Llonga, a beautiful Ibizan resort-town with one
+                of the island’s largest beaches, a picturesque setting and
+                plenty of things to do in town. The four-bedroom villa itself
+                adopts a contemporary style washed in white to accompany its
+                beautiful modern interior. Guests will be able to enjoy the
+                stunning panoramic views of the surrounding hills that evoke a
+                sense of privacy amongst nature. The spacious, open-plan design
+                makes spending time inside a pleasure while the expansive glass
+                windows and sliding doors allow a great deal of light to shine
+                through from the famous Ibizan sun. The luminous living area is
+                luxuriously spacious, featuring a large, ultra-comfortable sofa,
+                flat screen television attached to the wall and various dining
+                tables. The American-style open kitchen features a bench island
+                and is fully equipped with any appliance that one may need
+                during their stay, and has access to the pool. The villa’s
+                exterior is a perfect setting for spending quality time outside,
+                featuring a gorgeous long pool with access from two bedrooms and
+                the living room. The property also features a landscaped garden
+                and a pine forest which contrast the beauty of the natural
+                environment with the modernity of the luxurious villa. The
+                family-friendly area boasts access to parasailing, a
+                scuba-diving school, windsurfing and water-skiing activities for
+                those that plan on keeping active on vacation while being
+                strategically located close to Ibiza town for those who want to
+                experience the famous Ibizan nightlife. Cala Llonga manages to
+                maintain a quieter, more relaxed atmosphere, making it a perfect
+                option for those who need a relaxing escape but also want plenty
+                of fun options nearby.
+              </p>
+              <h2>Features</h2>div
+              <p>
+                MAID, PRIVATE POOL, AIR CONDITIONING, HOUSEKEEPING, WI-FI,
+                BARBECUE and TV
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+
+      <section>
+        <div>
+          <img src={cpk} alt="image" class="cpk" />
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <img src={cpl} alt="image" class="cpl" />
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <img src={cpb} alt="image" class="cpb" />
+        </div>
+      </section>
     </div>
   );
 };
