9c1f27fc9444e0fc0fcdd5865a972f2fcc9b3ff5
symonne taylor <safiya@symonnes-mbp.home>
2021-01-26 00:16:19 +0000

    banner pic

--- client/src/Home.js
+++ client/src/Home.js
@@ -23,7 +23,7 @@ function Home() {
         </Card>
 
         <Card
-          src="https://images.unsplash.com/photo-1581974206939-b42731ea9dc9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80"
+          src="https://images.unsplash.com/photo-1599809275671-b5942cabc7a2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2850&q=80"
           title="Sa Calma"
           description="Deluxe villa with spacious rooms."
           url="/saCalma"
@@ -37,21 +37,21 @@ function Home() {
       </div>
       <div className="home__section">
         <Card
-          src="https://images.unsplash.com/photo-1590398877831-1084ac6edb63?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1567&q=80"
+          src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1954&q=80"
           title="Villa Anna"
           description="Beautiful villa with a stunning views"
           price="£558/night"
           url="/villaAnna"
         />
         <Card
-          src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2850&q=80"
+          src="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
           title="San Lorenzo"
           description="stylish contemporary villa"
           price="£350/night"
           url="/sanLorenzo"
         />
         <Card
-          src="https://images.unsplash.com/photo-1570057633897-58005bd9788f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+          src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2850&q=80"
           title="Villa Atzi "
           description="Beautiful modern villa"
           price="£428/night"
